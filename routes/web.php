9cb57f2365035d6039da5885eb90021c77b8a75b
Mantas <6524518+mantas6@users.noreply.github.com>
2026-09-10 13:45:51 +0300

    Upd

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\JiraAttachmentController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
-
 Route::get('/jira/attachment', JiraAttachmentController::class)
     ->middleware('auth')
     ->name('jira.attachment');
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,16 @@
 <?php
 
-test('returns a successful response', function () {
-    $response = $this->get('/');
+test('the root route redirects guests to the filament login', function () {
+    $this->get('/')
+        ->assertRedirect('/login');
+});
 
-    $response->assertOk();
+test('the filament login displays the application brand', function () {
+    $this->get('/login')
+        ->assertSee('Tasks');
+});
+
+test('the legacy admin prefix is not registered', function () {
+    $this->get('/admin')
+        ->assertNotFound();
 });
