6c3a0f8b6c6169be991bbc40096f99b1d589611f
Mantas <6524518+mantas6@users.noreply.github.com>
2026-09-14 08:58:19 +0300

    Run jira:sync cron without weekday and work hour limits

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Console\Scheduling\Schedule as Scheduling;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -11,11 +10,4 @@ Artisan::command('inspire', function () {
 
 Schedule::command('jira:sync')
     ->everyFiveMinutes()
-    ->days([
-        Scheduling::MONDAY,
-        Scheduling::TUESDAY,
-        Scheduling::WEDNESDAY,
-        Scheduling::THURSDAY,
-    ])
-    ->between('8:00', '16:00')
     ->withoutOverlapping();
